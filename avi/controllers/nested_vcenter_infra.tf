5fa8f0106c6fa6310d1cfef8e729ee33dc6fff1b
tacobayle <nicolas.bayle@gmail.com>
2022-03-07 08:06:49 +0000

    20220303_avi_create

--- avi/controllers/nested_vcenter_infra.tf
+++ avi/controllers/nested_vcenter_infra.tf
@@ -1,23 +1,22 @@
 data "vsphere_datacenter" "dc_nested" {
-//  depends_on = [null_resource.dual_uplink_update_multiple_vds, null_resource.dual_uplink_update_single_vds]
-  count            = (var.avi.controller.create == true || var.avi.content_library.create == true ? 1 : 0)
+  count            = var.avi.controller.create == true || var.avi.content_library.create == true ? 1 : 0
   name = var.vcenter.datacenter
 }
 
 data "vsphere_compute_cluster" "compute_cluster_nested" {
-  count            = (var.avi.controller.create == true || var.avi.content_library.create == true ? 1 : 0)
+  count            = var.avi.controller.create == true || var.avi.content_library.create == true ? 1 : 0
   name          = var.vcenter.cluster
   datacenter_id = data.vsphere_datacenter.dc_nested[0].id
 }
 
 data "vsphere_datastore" "datastore_nested" {
-  count            = (var.avi.controller.create == true || var.avi.content_library.create == true ? 1 : 0)
+  count            = var.avi.controller.create == true || var.avi.content_library.create == true ? 1 : 0
   name = "vsanDatastore"
   datacenter_id = data.vsphere_datacenter.dc_nested[0].id
 }
 
 data "vsphere_resource_pool" "resource_pool_nested" {
-  count            = (var.avi.controller.create == true || var.avi.content_library.create == true ? 1 : 0)
+  count            = var.avi.controller.create == true || var.avi.content_library.create == true ? 1 : 0
   name          = "${var.vcenter.cluster}/Resources"
   datacenter_id = data.vsphere_datacenter.dc_nested[0].id
 }
@@ -42,4 +41,3 @@ resource "vsphere_content_library_item" "nested_library_avi_item" {
   library_id  = vsphere_content_library.nested_library_avi[0].id
   file_url = var.avi.content_library.ova_location
 }
-
