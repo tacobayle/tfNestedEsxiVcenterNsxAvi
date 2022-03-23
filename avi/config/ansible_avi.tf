61bb2acaae2950261b6a00b78ba21b52f5750867
tacobayle <nicolas.bayle@gmail.com>
2022-03-23 12:26:02 +0000

    20220322_avi_config

--- avi/config/ansible_avi.tf
+++ avi/config/ansible_avi.tf
@@ -7,7 +7,31 @@ resource "null_resource" "ansible_hosts_avi_header_1" {
 resource "null_resource" "ansible_hosts_avi_controllers" {
   depends_on = [null_resource.ansible_hosts_avi_header_1]
   provisioner "local-exec" {
-    command = "echo '        ${cidrhost(var.nsx.config.segments_overlay[0].cidr, var.nsx.config.segments_overlay[count.index].avi_controller)}:' | tee -a hosts_avi "
+    command = "echo '        ${cidrhost(var.nsx.config.segments_overlay[0].cidr, var.nsx.config.segments_overlay[0].avi_controller)}:' | tee -a hosts_avi "
+  }
+}
+
+data "template_file" "values" {
+  template = file("templates/values.yml.template")
+  vars = {
+    avi_version = var.avi.controller.version
+    controllerPrivateIp = cidrhost(var.nsx.config.segments_overlay[0].cidr, var.nsx.config.segments_overlay[0].avi_controller)
+    avi_old_password =  jsonencode(var.avi_old_password)
+    avi_password = jsonencode(var.avi_password)
+    avi_username = jsonencode(var.avi_username)
+    ntp = var.ntp.server
+    dns = var.dns.nameserver
+    nsx_password = var.nsx_password
+    nsx_server = var.vcenter.dvs.portgroup.management.nsx_ip
+    domain = var.dns.domain
+    transport_zone_name = var.avi.config.transport_zone_name
+    network_management = jsonencode(var.avi.config.network_management)
+    networks_data = jsonencode(var.avi.config.networks_data)
+    networks_backend = jsonencode(var.avi.config.networks_backend)
+    sso_domain = var.vcenter.sso.domain_name
+    vcenter_password = var.vcenter_password
+    vcenter_ip = var.vcenter.dvs.portgroup.management.vcenter_ip
+    content_library = var.avi.config.content_library_avi
   }
 }
 
@@ -27,11 +51,17 @@ resource "null_resource" "ansible_avi" {
     destination = "hosts_avi"
   }
 
+  provisioner "file" {
+    content = data.template_file.values.rendered
+    destination = "values.yml"
+  }
+
   provisioner "remote-exec" {
     inline = [
       "git clone ${var.avi.config.avi_config_repo} --branch ${var.avi.config.avi_config_tag}",
       "cd ${split("/", var.avi.config.avi_config_repo)[4]}",
-      "ansible-playbook -i ../hosts_avi local.yml --extra-vars '{\"avi_version\": ${jsonencode(var.avi.controller.version)}, \"controllerPrivateIps\": [${cidrhost(var.nsx.config.segments_overlay[0].cidr, var.nsx.config.segments_overlay[count.index].avi_controller)}], \"avi_password\": ${jsonencode(var.avi_password)}, \"avi_username\": ${jsonencode(var.avi_username)}, \"controller\": {\"cluster\": false, \"ntp\": [${jsonencode(var.dns.server)}], \"dns\": [${jsonencode(var.dns.nameserver)}]}, \"nsx_username\": \"admin\", \"nsx_password\": ${jsonencode(var.nsx_password)}, \"nsx_server\": ${jsonencode(var.vcenter.dvs.portgroup.management.nsx_ip)}, \"nsx\": {\"domains\": [${jsonencode(var.dns.domain)}], \"transport_zone\": {\"name\": ${jsonencode(var.avi.controller.transport_zone_name)} }, \"network_management\": ${jsonencode(var.avi.controller.config.network_management)}, \"networks_data\": ${jsonencode(var.avi.controller.config.networks_data)} },\"vcenter_credentials\": [{\"username\": \"administrator@${var.vcenter.sso.domain_name}\", \"password\": ${jsonencode(var.vcenter_password)}}]}'"
+      "ansible-playbook -i ../hosts_avi local.yml --extra-vars @../values.yml"
+//      "ansible-playbook -i ../hosts_avi local.yml --extra-vars '{\"avi_version\": ${jsonencode(var.avi.controller.version)}, \"controllerPrivateIps\": [${cidrhost(var.nsx.config.segments_overlay[0].cidr, var.nsx.config.segments_overlay[0].avi_controller)}], \"avi_old_password\": ${jsonencode(var.avi_old_password)}, \"avi_password\": ${jsonencode(var.avi_password)}, \"avi_username\": ${jsonencode(var.avi_username)}, \"controller\": {\"aviCredsJsonFile\": \"~/.creds.json\", \"environment\": \"vCenter\", \"cluster\": false, \"ntp\": [${jsonencode(var.ntp.server)}], \"dns\": [${jsonencode(var.dns.nameserver)}]}, \"nsx_username\": \"admin\", \"nsx_password\": ${jsonencode(var.nsx_password)}, \"nsx_server\": ${jsonencode(var.vcenter.dvs.portgroup.management.nsx_ip)}, \"nsxt\": {\"name\": \"dc1_nsxt\", \"domains\": [{\"name\": ${jsonencode(var.dns.domain)}}], \"transport_zone\": {\"name\": ${jsonencode(var.avi.config.transport_zone_name)} }, \"network_management\": ${jsonencode(var.avi.config.network_management)}, \"networks_data\": ${jsonencode(var.avi.config.networks_data)}, \"networks_backend\": ${jsonencode(var.avi.config.networks_backend)} }, \"vcenter_credentials\": [{\"username\": \"administrator@${var.vcenter.sso.domain_name}\", \"password\": ${jsonencode(var.vcenter_password)}}]}'"
     ]
   }
 }
--- avi/config/variables.tf
+++ avi/config/variables.tf
@@ -5,6 +5,7 @@ variable "vcenter" {}
 variable "external_gw" {}
 variable "avi" {}
 variable "avi_password" {}
+variable "avi_old_password" {}
 variable "avi_username" {}
 variable "nsx_password" {}
 variable "vcenter_password" {}
